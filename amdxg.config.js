0676716aa1a4c96172e5d37c7d44a714382a3e3a
fukata <tatsuya.fukata@gmail.com>
2020-05-24 17:50:33 +0900

    change repository url

--- amdxg.config.js
+++ amdxg.config.js
@@ -5,7 +5,7 @@ module.exports = {
   authorLink: "https://twitter.com/fukata",
   gtag: "UA-148224679-1",
   lang: "ja",
-  repository: "https://github.com/fukata/www.fukata.dev",
+  repository: "fukata/www.fukata.dev",
   socialShare: {
     twitter: true,
     hatena: true,
